e29ae0d5b5b863ea8d9c020e640645cdbf79eeaa
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-03-16 09:40:39 +0530

    gitignore

---
+++ algo-search-and-sort/com/a04t/search_2_binary/RunnerMain.java
@@ -0,0 +1,10 @@
+package com.a04t.search_2_binary;
+
+public class RunnerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.");
+	}
+
+}
