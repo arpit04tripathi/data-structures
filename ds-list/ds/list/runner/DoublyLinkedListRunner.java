26b1879e5f51e1255609ded3e1f29dd1be39ce07
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-04-11 21:27:49 +0530

    Linked List

---
+++ ds-list/ds/list/runner/DoublyLinkedListRunner.java
@@ -0,0 +1,13 @@
+package ds.list.runner;
+
+import ds.list.doublyLinkedList.DoublyLinkedList;
+
+public class DoublyLinkedListRunner {
+
+	public static void main(String[] args) {
+		DoublyLinkedList dll = new DoublyLinkedList();
+		dll.addFirst(50).addFirst(68).addFirst(29).addFirst(23).addLast(99);
+		dll.print();
+		dll.reversePrint();
+	}
+}
--- ds-list/ds/list/singlyLinkedList/SinglyLinkedList.java
+++ ds-list/ds/list/singlyLinkedList/SinglyLinkedList.java
@@ -24,7 +24,7 @@ public class SinglyLinkedList {
 		this.head = head;
 	}
 
-	public void addFirst(int data) {
+	public SinglyLinkedList addFirst(int data) {
 		SLLNode temp = new SLLNode(data);
 		if (head == null) {
 			this.head = temp;
@@ -33,15 +33,26 @@ public class SinglyLinkedList {
 			this.head = temp;
 		}
 		length++;
+		return this;
 	}
 
-	public void addLast(int data) {
+	public SinglyLinkedList addLast(int data) {
 		SLLNode last = getLastNode();
 		last.setNext(new SLLNode(data));
+		return (this);
 	}
 
 	public void insertAt(int index, int data) {
-
+		if(index == 0) {
+			addFirst(data);
+		}else if(index > length) {
+			addLast(data);
+		}else {
+			SLLNode temp = getNodeAt(index-1);
+			SLLNode node = new SLLNode(data);
+			node.setNext(temp.getNext());
+			temp.setNext(node);
+		}
 	}
 
 	public void deleteFirst() {
@@ -50,6 +61,11 @@ public class SinglyLinkedList {
 
 	public void deleteLast(int data) {
 
+	}
+	
+	public void deleteAt(int index) {
+		SLLNode temp = getNodeAt(index-1);
+		temp.next = temp.next.next;
 	}
 
 	public SLLNode getLastNode() {
@@ -61,10 +77,47 @@ public class SinglyLinkedList {
 		return temp;
 	}
 
-	public int getLastNodeData() {
-		return getLastNode().getData();
+	public SLLNode getNodeAt(int index) {
+		SLLNode temp = head;
+		if(temp == null)
+			return null;
+		for(int i=0; i<index; i++) {
+			temp = temp.getNext();
+		}
+		return temp;
 	}
 	
+	public void reverseIterative() {
+		if(head == null || head.next == null)
+			return;
+		SLLNode prev = null;
+		SLLNode curr = head;
+		SLLNode next = head.next;
+		while(curr.next != null) {
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+			next = curr.next;
+		}
+		curr.next = prev;
+		head = curr;
+	}
+	
+	public void reverseRecursive() {
+		reverseRecursive(head);
+	}
+	
+	private void reverseRecursive(SLLNode node) {
+		if(isLastNode(node)) {
+			head = node;
+			return;
+		}
+		reverseRecursive(node.next);
+		SLLNode next = node.next;
+		node.next = null;
+		next.next = node;
+	}
+
 	public void print() {
 		SLLNode node = this.head;
 		StringBuilder sb = new StringBuilder();
@@ -74,5 +127,32 @@ public class SinglyLinkedList {
 		}
 		sb.append("null");
 		System.out.println(sb.toString());
+	}
+	
+	public void printRecursive() {
+		printForwardRecursive(head);
+		printReverseRecursive(head);
+		System.out.println("null");
+	}
+
+	private void printForwardRecursive(SLLNode node) {
+		if(node == null) {
+			System.out.println("null");
+			return;
+		}
+		System.out.print(String.format("%d --> ", node.data));
+		printForwardRecursive(node.next);
+	}
+	
+	private void printReverseRecursive(SLLNode node) {
+		if(node == null) {
+			return;
+		}
+		printReverseRecursive(node.next);
+		System.out.print(String.format("%d --> ", node.data));
+	}
+
+	private boolean isLastNode(SLLNode node) {
+		return node.next == null;
 	}
 }
