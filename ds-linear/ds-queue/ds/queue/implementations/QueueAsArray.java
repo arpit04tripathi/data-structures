26b3b7b3b9271fd62323bfda028cf32afb3a0e0b
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-04-13 08:46:43 +0530

    queue

---
+++ ds-linear/ds-queue/ds/queue/implementations/QueueAsArray.java
@@ -0,0 +1,52 @@
+package ds.queue.implementations;
+
+public class QueueAsArray {
+	int[] arr;
+	int front = -1;
+	int rear = -1;
+	
+	public QueueAsArray(int size) {
+		arr = new int[size];
+	}
+
+	public boolean isEmpty() {
+		return (front==-1 && rear==-1);
+	}
+	
+	public void enqueue(int item) {
+		if(isFull()) {
+			System.err.println("Error : Queue is full");
+			return;
+		}
+		if(isEmpty()) {
+			front = rear = 0;
+			arr[front]=item;
+		}else {
+			arr[++rear]=item;
+		}
+	}
+	
+	public int dequeue() {
+		int value = arr[0];
+		if(isEmpty()) {
+			System.err.println("Error : Queue is Empty.");
+		} else if(front == rear && front == 0) {
+			value = arr[front];
+			front = rear = -1;
+		}else {
+			value = arr[front++];
+		}
+		return value;
+	}
+
+	private boolean isFull() {
+		return rear == arr.length-1;
+	}
+	
+	public void print() {
+		System.out.print("queue : ");
+		for(int i=front ; i<=rear; i++)
+			System.out.print(String.format("%d <-- ", arr[i]));
+		System.out.println();
+	}
+}
---
+++ ds-linear/ds-queue/ds/queue/runner/QueueRunner.java
@@ -0,0 +1,42 @@
+package ds.queue.runner;
+
+import ds.queue.implementations.CircularQueueAsArray;
+import ds.queue.implementations.QueueAsArray;
+
+public class QueueRunner {
+
+	public static void main(String[] args) {
+		
+//		runQueueAsArray();
+		runCircularQueueAsArray();
+	}
+
+	private static void runCircularQueueAsArray() {
+		CircularQueueAsArray q = new CircularQueueAsArray(5);
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.enqueue(50);
+		q.enqueue(60);
+		q.print();
+		q.dequeue();
+		q.enqueue(60);
+		q.print();
+	}
+
+	private static void runQueueAsArray() {
+		QueueAsArray q = new QueueAsArray(5);
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.enqueue(50);
+		q.enqueue(60);
+		q.print();
+		q.dequeue();
+		q.enqueue(60);
+		q.print();
+	}
+
+}
