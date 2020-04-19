7983e9d04038270a66d8e9a6852625ce0debe9b8
Arpit Tripathi <arpit04tripathi@gmail.com>
2020-04-20 00:24:53 +0530

    tree

--- ds-linear/ds-queue/ds/queue/implementations/QueueAsArray.java
+++ ds-linear/ds-queue/ds/queue/implementations/QueueAsArray.java
@@ -4,49 +4,50 @@ public class QueueAsArray {
 	int[] arr;
 	int front = -1;
 	int rear = -1;
-	
+
 	public QueueAsArray(int size) {
-		arr = new int[size];
+		this.arr = new int[size];
 	}
 
-	public boolean isEmpty() {
-		return (front==-1 && rear==-1);
-	}
-	
-	public void enqueue(int item) {
-		if(isFull()) {
-			System.err.println("Error : Queue is full");
-			return;
-		}
-		if(isEmpty()) {
-			front = rear = 0;
-			arr[front]=item;
-		}else {
-			arr[++rear]=item;
-		}
-	}
-	
 	public int dequeue() {
-		int value = arr[0];
-		if(isEmpty()) {
+		int value = this.arr[0];
+		if (isEmpty()) {
 			System.err.println("Error : Queue is Empty.");
-		} else if(front == rear && front == 0) {
-			value = arr[front];
-			front = rear = -1;
-		}else {
-			value = arr[front++];
+		} else if ((this.front == this.rear) && (this.front == 0)) {
+			value = this.arr[this.front];
+			this.front = this.rear = -1;
+		} else {
+			value = this.arr[this.front++];
 		}
 		return value;
 	}
 
-	private boolean isFull() {
-		return rear == arr.length-1;
+	public void enqueue(int item) {
+		if (isFull()) {
+			System.err.println("Error : Queue is full");
+			return;
+		}
+		if (isEmpty()) {
+			this.front = this.rear = 0;
+			this.arr[this.front] = item;
+		} else {
+			this.arr[++this.rear] = item;
+		}
 	}
-	
+
+	public boolean isEmpty() {
+		return ((this.front == -1) && (this.rear == -1));
+	}
+
+	private boolean isFull() {
+		return this.rear == (this.arr.length - 1);
+	}
+
 	public void print() {
 		StringBuilder sb = new StringBuilder("queue : ");
-		for(int i=front ; i<=rear; i++)
-			sb.append(String.format("%d <-- ", arr[i]));
+		for (int i = this.front; i <= this.rear; i++) {
+			sb.append(String.format("%d <-- ", this.arr[i]));
+		}
 		System.out.println(sb.toString());
 	}
 }
--- ds-linear/ds-queue/ds/queue/runner/QueueRunner.java
+++ ds-linear/ds-queue/ds/queue/runner/QueueRunner.java
@@ -1,16 +1,30 @@
 package ds.queue.runner;
 
-import java.util.concurrent.TimeUnit;
-
 import ds.queue.implementations.CircularQueueAsArray;
 import ds.queue.implementations.QueueAsArray;
+import ds.queue.implementations.QueueAsLinkedList;
 
 public class QueueRunner {
 
 	public static void main(String[] args) {
-		
+
 		runQueueAsArray();
 		runCircularQueueAsArray();
+		runQueueAsLinkedList();
+	}
+
+	private static void runCircularQueueAsArray() {
+		CircularQueueAsArray q = new CircularQueueAsArray(5);
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.enqueue(50);
+		q.enqueue(60);
+		q.print();
+		q.dequeue();
+		q.enqueue(60);
+		q.print();
 	}
 
 	private static void runQueueAsArray() {
@@ -27,16 +41,17 @@ public class QueueRunner {
 		q.print();
 	}
 
-	private static void runCircularQueueAsArray() {
-		CircularQueueAsArray q = new CircularQueueAsArray(5);
+	private static void runQueueAsLinkedList() {
+		QueueAsLinkedList q = new QueueAsLinkedList();
 		q.enqueue(10);
 		q.enqueue(20);
+		q.print();
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
 		q.enqueue(30);
 		q.enqueue(40);
 		q.enqueue(50);
-		q.enqueue(60);
-		q.print();
-		q.dequeue();
 		q.enqueue(60);
 		q.print();
 	}
